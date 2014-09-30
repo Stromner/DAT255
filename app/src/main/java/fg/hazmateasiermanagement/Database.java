e57f1092663b546c470878482caff5b54723a8fb
Stromner <davidstromner@gmail.com>
2014-09-30 10:50:28 +0200

    Modified skeleton code for database

--- app/src/main/java/fg/hazmateasiermanagement/Database.java
+++ app/src/main/java/fg/hazmateasiermanagement/Database.java
@@ -1,4 +1,8 @@
-package fg.hazmateasiermanagement;
+﻿package fg.hazmateasiermanagement;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
 import java.util.List;
 import fg.hazmateasiermanagement.Element;
@@ -14,20 +18,67 @@ import fg.hazmateasiermanagement.Element;
  * @version 2014-09-24
  */
 
-public class Database {
+public class Database extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "HazmatDatabase.db";
+
+    /**
+     *
+     * @param context
+     */
+    public Database(Context context){
+        super(context, DATABASE_NAME , null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
+
+    }
+
+    /**
+     *
+     * @param element
+     * @return
+     */
     public Boolean addElement(Element element){
         return false;
     }
+
+    /**
+     *
+     * @param element
+     * @return
+     */
     public Boolean removeElement(Element element){
-	//Testing changing something in method
         return false;
     }
+
+    /**
+     *
+     * @param position
+     * @return
+     */
     public Element getElement(int position){
         return null;
     }
+
+    /**
+     *
+     * @param database
+     * @return
+     */
     public Boolean checkList(Database database){
         return false;
     }
+
+    /**
+     *
+     * @return
+     */
     public List getList(){
         return null;
     }
