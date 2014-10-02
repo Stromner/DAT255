311f0d24ae8e888efffb82b9639cbf86d078df4f
MagnusK <magnus__k@hotmail.com>
2014-10-02 15:39:49 +0200

    Correct push with tabs. Search/Current/HistoryTab classes are useless atm, might want later.

---
+++ app/src/main/java/fg/hazmateasiermanagement/SearchTab.java
@@ -0,0 +1,29 @@
+package fg.hazmateasiermanagement;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TextView;
+
+/**
+ * Created by Magnus on 2014-10-01.
+ * Currently useless, just leaving it here incase things change.
+ */
+public class SearchTab extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        /**
+        setContentView(R.layout.activity_search);
+
+        TextView tv = new TextView(this);
+        tv.setTextSize(25);
+        tv.setGravity(Gravity.CENTER_VERTICAL);
+        tv.setText("This Is Search Activity");
+
+        setContentView(tv);
+         */
+    }
+}
