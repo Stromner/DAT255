6a4c124758e7b99006c5df2240778f65accdd411
Voxxorian <Benjamin.wijk@gmail.com>
2014-10-05 20:09:41 +0200

    Search prototype, not functioning

---
+++ app/src/main/java/fg/hazmateasiermanagement/SearchDatabase.java
@@ -0,0 +1,44 @@
+package fg.hazmateasiermanagement;
+
+import android.app.Activity;
+import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.widget.SearchView;
+import android.widget.TextView;
+/**
+ * Created by Benjamin on 2014-10-05.
+ */
+public class SearchDatabase {
+    private void initSearchView() {
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        final SearchView searchView = (SearchView) findViewById(R.id.searchView);
+        SearchableInfo searchableInfo = searchManager.getSearchableInfo(getComponentName());
+        searchView.setSearchableInfo(searchableInfo);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        if (ContactsContract.Intents.SEARCH_SUGGESTION_CLICKED.equals(intent.getAction())) {
+            String displayName = getContactName(intent);
+            displayText.setText(displayName);
+        } else if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            displayText.setText("search for: '" + query + "'...");
+        }
+    }
+
+    private String getContactName(Intent intent) {
+        Cursor phoneCursor = getContentResolver().query(intent.getData(), null, null, null, null);
+        phoneCursor.moveToFirst();
+        int colNameIndex = phoneCursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+        String contactName = phoneCursor.getString(colNameIndex);
+        phoneCursor.close();
+        return contactName;
+    }
+
+}
