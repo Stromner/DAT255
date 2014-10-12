169748eb4e86bf18c201575a107cc3e8b8a369d1
Stromner <davidstromner@gmail.com>
2014-10-12 18:11:24 +0200

    Added clone method to Element

--- app/src/main/java/fg/hazmateasiermanagement/Element.java
+++ app/src/main/java/fg/hazmateasiermanagement/Element.java
@@ -12,28 +12,17 @@ import android.media.Image;
  * @version 2014-10-09
  */
 
-public class Element {
-    private int unNumber;           //UN number
-    private String name;            //Name of the element
-    private String description;     //Describing the Element in detail
-    private int classNumber;        //Class number that covers dangerous substance or article
-    private String packingGroup;    //Packing Group I II or III
-    private String label;           //labels used to show what material that can be shipped together.
-    private int weight;             //The weight of the material
-    private String hazmatImage;     //String that contains the name of the image
+public class Element implements Cloneable{
+    private int unNumber;       //UN number
+    private String name;        //Name of the element
+    private String description; //Describing the Element in detail
+    private int classNumber;    //Class number that covers dangerous substance or article
+    private String packingGroup;//Packing Group I II or III
+    private String label;       //labels used to show what material that can be shipped together.
+    private float weight;       //The weight of the material
+    private String hazmatImage; //String that contains the name of the image
 
-    /**
-     * Constructor with all necessary fields included.
-     * @param unNumber
-     * @param name
-     * @param description
-     * @param classNumber
-     * @param packingGroup
-     * @param label
-     * @param weight;
-     * @param hazmatImage;
-     */
-    public Element(int unNumber, String name, String description, int classNumber, String packingGroup, String label, int weight, String hazmatImage){         //Full constructor
+    public Element(int unNumber, String name, String description, int classNumber, String packingGroup, String label, int weight, String hazmatImage){
         this.unNumber = unNumber;
         this.name = name;
         this.description = description;
@@ -45,155 +34,57 @@ public class Element {
     }
 
     /**
-     * Constructor with only unNumber field, used for testing.
+     * Minimal constructor with only the number of the element, used for testing.
      * @param unNumber
      */
     public Element(int unNumber){    //Minimum constructor
         this.unNumber = unNumber;
     }
 
-    /**
-     *
-     * @param unNumber
-     * @return true if unNumber has been set to new value, null ow
-     */
-    public boolean setUNNumber(int unNumber){
-        this.unNumber = unNumber;
-        return this.unNumber == unNumber ? true:false;
-    }
-
-    /**
-     *
-     * @return unNumber if exist null ow.
-     */
     public int getUNNumber(){
         return unNumber;
     }
 
-    /**
-     *
-     * @param name
-     * @return true if name has been set to new value, null ow
-     */
-    public boolean setName(String name){
-        this.name = name;
-        return this.name == name ? true:false;
-    }
-    /**
-     *
-     * @return name if exist, null ow.
-     */
     public String getName(){
         return name;
     }
 
-    /**
-     *
-     * @param description
-     * @return true if description has been set to new value, null ow
-     */
-    public boolean setDescription(String description){
-        this.description = description;
-        return this.description == description ? true:false;
-    }
-
-    /**
-     *
-     * @return description if exist, null ow
-     */
     public String getDescription(){ return description;}
 
-    /**
-     *
-     * @param classNumber
-     * @return true if classNumber has been set to new value, false ow
-     */
-    public boolean setClassNumber(int classNumber){
-        this.classNumber = classNumber;
-        return this.classNumber == classNumber ? true:false;
-    }
-
-    /**
-     *
-     * @return classNumber if exist, null ow.
-     */
     public int getClassNumber(){
        return classNumber;
     }
 
-    /**
-     *
-     * @return true if packinggroup has been set to new value, flase ow
-     */
-    public boolean setPackingGroup(){
-        this.packingGroup = packingGroup;
-        return this.packingGroup == packingGroup ? true:false;
-    }
-
-    /**
-     *
-     * @return packingGroup if exist, null ow.
-     */
     public String getPackingGroup(){
         return packingGroup;
     }
 
-    /**
-     *
-     * @param label
-     * @return true if label has been set to new, false ow
-     */
-    public boolean setLabel(String label){
-        this.label = label;
-        return this.label == label ? true:false;
-    }
-
-    /**
-     *
-     * @return label if exist, null ow
-     */
     public String getLabel(){
         return label;
     }
 
-    /**
-     *
-     * @return weight if exist, null ow
-     */
-    public int getWeight(){
+    public float getWeight(){
         return weight;
     }
-    public Boolean setWeight(int weight){
+
+    public Boolean setWeight(float weight){
         this.weight = weight;
 
         return this.weight == weight ? true:false;
     }
 
-    /**
-     *
-     * @return hazmatImage if exist, null ow
-     */
     public String getHazmatImage(){
         return hazmatImage;
     }
 
-    /**
-     *
-     * @param hazmatImage
-     * @return true if created, false ow
-     */
     public boolean setHazmatImage(String hazmatImage){
         this.hazmatImage = hazmatImage;
-        return this.hazmatImage == hazmatImage ? true:false;
+        return this.hazmatImage.compareTo(hazmatImage) == 0 ? true:false;
     }
 
     @Override
-    public Object clone(){
-        return null;
-    }
-
-    @Override
-    public int hashCode(){
-       return 0;
+    public Object clone() throws CloneNotSupportedException{
+        // Since strings are immutable and any other variable in this class is of simple format calling super is enough.
+        return super.clone();
     }
 }
